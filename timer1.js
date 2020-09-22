0b2031b90666e1daf9174be841e6e8342e659ba3
Jason Vongsana <jvongsana@hotmail.com>
2020-09-22 20:56:03 +0000

    Revised with setInterval and setTimeout

--- timer1.js
+++ timer1.js
@@ -1,6 +1,5 @@
 let input = process.argv.slice(2);
 let timeArr = [];
-let count = 0;
 
 for (let i = 0; i < input.length; i++) {
   if (input[i] > 0) {
@@ -10,18 +9,23 @@ for (let i = 0; i < input.length; i++) {
 timeArr.sort(function(a, b) {
   return a - b;
 });
+// Uses setTimeout (ignore line 13-17 if testing setInterval)
+for (let i = 0; i < timeArr.length; i++) {
+  setTimeout(() => {
+    process.stdout.write(`beep\n`);
+  }, timeArr[i] * 1000);
+}
 
-let position = 0;
-
-let clock = setInterval(() => {
-  if (position === timeArr.length) {
-    clearInterval(clock);
-    return '\n';
-  } else if (timeArr[position] === count) {
-    process.stdout.write(`beep`);
-    console.log(count * 1000);
-    position += 1;
-  }
-  count++;
-}, 1000);
-
+// same outcome but with setInterval (ignore line 19-31 if testing setTimeout)
+// let count = 0;
+// let position = 0;
+// let clock = setInterval(() => {
+//   if (position === timeArr.length) {
+//     clearInterval(clock);
+//     return '\n';
+//   } else if (timeArr[position] === count) {
+//     process.stdout.write(`beep`);
+//     position++;
+//   }
+//   count++;
+// }, 1000);
